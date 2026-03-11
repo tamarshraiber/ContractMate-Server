ceb0262e75cf448921e620c40f87aee12a807131
Efrat Friedman <efratfriedman18@gmail.com>
2026-03-11 22:59:41 +0200

    enhance file upload and query endpoints with session management and error handling

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # FastAPI app, endpoints
 
 # main.py
-from fastapi import FastAPI, File, UploadFile, Body
+from fastapi import FastAPI, File, UploadFile, Body, HTTPException
 from utils.file_handler import save_uploaded_file
 from utils.text_extraction import extract_text_from_pdf, extract_text_from_docx
 from utils.text_splitter import split_text_into_chunks
+from session_manager import create_session ,get_session, cleanup_expired_sessions
+from rag.rag_engine import get_answer_from_chunks
+import asyncio
 
 
 # כאן ניצור את האפליקציה
@@ -30,10 +33,12 @@ async def upload_file(file: UploadFile = File(...)):
     # חלוקה ל-chunks
     chunks = split_text_into_chunks(text)
 
+    # create session
+    session_id = create_session(chunks, file.filename)
+
     return {
         "message": "File uploaded successfully",
-        "path": file_path,
-        "extracted_text": text,
+        "session_id": session_id,
         "chunks_count": len(chunks)
     }
 
@@ -41,18 +46,29 @@ async def upload_file(file: UploadFile = File(...)):
 # Endpoint 2: Query / שאילתה על החוזה
 # --------------------------
 @app.post("/query")
-async def query_contract(query: str = Body(...)):
-    """
-    מקבל שאילתא מהמשתמש, לדוגמה:
-    "האם החוזה הזה הוגן?"
-    
-    בשלב זה נשתמש ב-mock response
-    כדי לבדוק את ה-flow.
-    """
-    # TODO: כאן נתחבר ל-RAG + OLLAMA
-    mock_response = f"זו תשובה לדוגמה על השאלה: '{query}'"
-    
-    return {"answer": mock_response}
+async def query_contract(
+    session_id: str =Body(...),
+    query: str = Body(...)
+):
+    session = get_session(session_id)
+    if not session:
+        raise HTTPException(status_code=404, detail="Session expired or file not uploaded")
+
+    answer = get_answer_from_chunks(query,session["chunks"], session["history"])
+    session["history"].append({"query":query, "answer":answer})
+
+
+    return {"answer": answer}
+
+
+@app.on_event("startup")
+async def start_cleanup_task():
+    async def cleanup_loop():
+        import time
+        while True:
+            cleanup_expired_sessions()
+        await asyncio.sleep(600) #every 10 minutes 
+    asyncio.create_task(cleanup_loop)       
 
 # --------------------------
 # להרצה מקומית:
